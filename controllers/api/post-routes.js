6fda8438754e3c4960921270cf1c5360384a8c6a
Abbush <glgberat@yahoo.com>
2022-03-27 00:34:58 -0400

    Auth middleware added to Routes

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const { Post, User, Comment } = require('../../models');
 const sequelize = require('../../config/connection');
-
+const withAuth = require('../../utils/auth');
 
 // get all users
 router.get('/', (req, res) => {
@@ -77,7 +77,7 @@ router.get('/', (req, res) => {
       });
   });
 
-router.post('/', (req, res) => {
+router.post('/', withAuth, (req, res) => {
     Post.create({
       title: req.body.title,
       post_content: req.body.post_content,
@@ -90,7 +90,7 @@ router.post('/', (req, res) => {
       });
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', withAuth, (req, res) => {
     Post.update({
         title: req.body.title,
         post_content: req.body.post_content
@@ -113,7 +113,7 @@ router.put('/:id', (req, res) => {
       });
   });
 
-  router.delete('/:id', (req, res) => {
+  router.delete('/:id', withAuth, (req, res) => {
     Post.destroy({
       where: {
         id: req.params.id
